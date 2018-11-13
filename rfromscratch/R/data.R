7b43c746123dc3ace23413f57be764c33c84ac3d
Sarah Gibson <sgibson@turing.ac.uk>
2018-11-13 14:42:59 +0000

    Added gender data file

---
+++ rfromscratch/R/data.R
@@ -0,0 +1,12 @@
+#' World Bank Gender Data
+#'
+#' The data we are analyzing are a subset from this dataset
+#' from the World Bank on gender and inequality.
+#'
+#' The subset is a selection of variables for every country. For each variable, we have taken the mean of all
+#' available values from 2012 through 2016.
+#'
+#' @format A data fram with 263 observations and 8 columns.
+#'
+#' @source \url{https://data.worldbank.org/data-catalog/gender-statistics}
+"gender_data"
